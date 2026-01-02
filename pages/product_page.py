c4c650fcdea90e82d3e1867edd491cc7864785b2
1nlbrn1 <86798663+1nlbrn1@users.noreply.github.com>
2026-01-02 21:46:07 +0500

    Parameterized the test for Product Page.  - updated product_page.py  - updated test_product_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -13,4 +13,4 @@ class ProductPage(BasePage):
     def should_be_added_to_basket(self, product_name, product_price):
         alert_product_name = self.browser.find_element(*ProductPageLocators.ALERT_PRODUCT_NAME).text
         alert_product_price = self.browser.find_element(*ProductPageLocators.ALERT_PRODUCT_PRICE).text
-        assert alert_product_name == product_name and alert_product_price == product_price
+        assert alert_product_name == product_name and alert_product_price == product_price, "No addition to the basket"
