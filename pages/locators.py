9e8c7915c5d5e58edc1c517eaa5a1b129fd9c1ff
1nlbrn1 <86798663+1nlbrn1@users.noreply.github.com>
2026-01-10 17:07:01 +0500

    Add new tests. Fixed a test. Implemented BasePageLocators.

--- pages/locators.py
+++ pages/locators.py
@@ -1,12 +1,17 @@
 from selenium.webdriver.common.by import By
 
 
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
 class LoginPageLocators:
-    REGISTRATION_FORM = (By.CSS_SELECTOR, "register_form")
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
 
 
@@ -17,4 +22,3 @@ class ProductPageLocators:
     ALERT_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages>.alert:nth-child(1)>div>strong")
     ALERT_PRODUCT_PRICE = (By.CSS_SELECTOR, "#messages>.alert:nth-child(3)>div>p>strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
-
--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 from pages.product_page import ProductPage
 import pytest
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 
 
 # @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -42,3 +42,15 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_to_basket()
     page.should_have_disappeared_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
